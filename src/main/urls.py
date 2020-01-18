b7daf280c753e1966b1609fe3f7f7132ef8c5460
Jordi Bagot <jordi.bagot@elements.nl>
2020-01-18 12:52:15 +0100

    Add k8s manifests

--- src/main/urls.py
+++ src/main/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.autodiscover()
+
+app_name = 'main'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
